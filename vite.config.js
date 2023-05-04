be9ba3814cf4f770f4f61a5afc58a90d5e70fde0
MathisHumbert <mathishumbert71@gmail.com>
2023-05-04 19:13:12 +0200

    created route for build in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 // vite.config.js
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [glsl()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        index2: resolve(__dirname, 'index2.html'),
+        index3: resolve(__dirname, 'index3.html'),
+      },
+    },
+  },
 });
